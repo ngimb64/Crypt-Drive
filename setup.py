f1a60192f1584838654b045e3789e6d9d073d8d4
Nick G <ngimbel64@protonmail.com>
2022-01-03 13:34:28 -0500

    1-3-22  Updated setup script

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def install(package, stdout, stderr, exec_time):
         outs, errs = command.communicate()
 
 def main():
-    re_mod = re.compile(r'^[a-zA-Z]{2,20}')
+    re_mod = re.compile(r'^[a-zA-Z0-9\=\-\.]{2,20}')
     # Open file & iterate line by line #
     try:
         with open('packages.txt', 'r') as file:
@@ -23,8 +23,8 @@ def main():
                 if re.search(re_mod, line) != None:
                     install(line, None, None, 2)
     # File error handling #
-    except (IOError, FileNotFoundError, Exception) as err:
-        print(f'* Error Occured: {err} *\n')
+    except (IOError, FileNotFoundError,Exception) as err:
+        print(f'\n* Error Occured: {err} *\n\n')
         input('Hit enter to end ..\n')
 
 if __name__ == '__main__':
