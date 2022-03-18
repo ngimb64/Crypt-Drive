137b53b5602206b1bb829428a50906a271ec54d7
Nicholas Gimbel <56981761+ngimb64@users.noreply.github.com>
2022-03-18 02:24:44 -0400

    Update - 3/18/22

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # Script for installing modules not included
 # with Python 3 default installation #
 
-import re
+import os, re
 from time import sleep
 from subprocess import Popen, SubprocessError, TimeoutExpired, CalledProcessError 
 
@@ -15,17 +15,20 @@ def install(package, stdout, stderr, exec_time):
 
 def main():
     re_mod = re.compile(r'^[a-zA-Z0-9\=\-\.]{2,20}')
+    filename = 'packages.txt'
+
     # Open file & iterate line by line #
-    try:
-        with open('packages.txt', 'r') as file:
-            for line in file:
-                # If regex matches package name .. install #
-                if re.search(re_mod, line) != None:
-                    install(line, None, None, 2)
-    # File error handling #
-    except (IOError, FileNotFoundError,Exception) as err:
-        print(f'\n* Error Occured: {err} *\n\n')
-        input('Hit enter to end ..\n')
+    if os.path.isfile(filename) and os.access(filename, os.R_OK):
+        try:
+            with open(filename, 'r') as file:
+                for line in file:
+                    # If regex matches package name .. install #
+                    if re.search(re_mod, line):
+                        install(line, None, None, 2)
+        # File error handling #
+        except (IOError, FileNotFoundError,Exception) as err:
+            print(f'\n* Error Occured: {err} *\n\n')
+            input('Hit enter to end ..\n')
 
 if __name__ == '__main__':
     try:
